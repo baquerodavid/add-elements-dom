cef348e83f2cd8a149e7fd384e631185b5e20376
David Baquero <davidbaquerogonzalez@gmail.com>
2025-11-01 20:24:36 +0100

    Ejercicio resuelto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
  // Aquí tu código
+
+const btn = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+btn.addEventListener('click', function () {
+  const newItem = prompt('Escribe el nuevo elemento:');
+  const nuevoElemento = document.createElement('li');
+  nuevoElemento.textContent = newItem;
+  lista.appendChild(nuevoElemento);
+});
