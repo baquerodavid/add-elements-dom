4c06d5fe0eb6e9be124ca19695321b446cb41007
David Baquero <davidbaquerogonzalez@gmail.com>
2025-11-05 21:58:11 +0100

    Code added from the live review

--- js/script.js
+++ js/script.js
@@ -9,3 +9,38 @@ btn.addEventListener('click', function () {
   nuevoElemento.textContent = newItem;
   lista.appendChild(nuevoElemento);
 });
+
+
+// Aquí el código hecho en la Live review. Condicional para que en caso que se cancele el prompt o se escriba un texto vacío (solo espacios), que no haga nada
+
+/* const lista = document.getElementById("lista")
+const agregar = document.getElementById("agregar")
+const input = document.getElementById("input") */
+
+// agregar.addEventListener("click", function() {
+//   const producto = prompt("Añade un producto")
+//   if(producto && producto.trim() !== "") {
+//     const li = document.createElement("li")
+//     li.textContent = producto
+//     lista.appendChild(li)
+//   }
+// })
+
+// Aquí lo hacemos con un input que se debe de añadir al HTML
+// agregar.addEventListener("click", function() {
+//   const producto = input.value
+//   if(producto && producto.trim() !== "") {
+//     const li = document.createElement("li")
+//     li.textContent = producto
+//     lista.appendChild(li)
+//     input.value = ""
+//   }
+// })
+
+/* agregar.addEventListener("click", function () {
+  const producto = input.value
+  if (producto && producto.trim() !== "") {
+    lista.innerHTML += `<li>${producto}</li>`
+    input.value = ""
+  }
+}) */
